1b211980f8252f510271bde638ee236db9b359b3
bernatskyi <bernatskyi@haulmont.com>
2020-09-30 20:21:34 +0100

    add RequestLoggingFilter for logging requests details and requests processing time statistic

--- src/main/java/com/cryfish/myalomatika/access/control/AccessServiceApplication.java
+++ src/main/java/com/cryfish/myalomatika/access/control/AccessServiceApplication.java
@@ -1,14 +1,11 @@
 package com.cryfish.myalomatika.access.control;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cryfish")
 public class AccessServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccessServiceApplication.class, args);
---
+++ src/main/java/com/cryfish/springboot/filter/logging/RequestLoggingFilter.java
@@ -0,0 +1,47 @@
+package com.cryfish.springboot.filter.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends AbstractRequestLoggingFilter {
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    public RequestLoggingFilter() {
+        setIncludeQueryString(true);
+        setIncludePayload(true);
+        setMaxPayloadLength(100000);
+        setIncludeHeaders(true);
+        setIncludeClientInfo(true);
+        setBeforeMessagePrefix("Got request: ");
+    }
+
+    @Override
+    protected void beforeRequest(HttpServletRequest request, String message) {
+        logger.info(message);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        long timestamp = System.currentTimeMillis();
+
+        super.doFilterInternal(request, response, filterChain);
+
+        logger.info("Process {} request '{}' in {} ms", request.getMethod(), request.getRequestURI(), System.currentTimeMillis() - timestamp);
+    }
+
+    @Override
+    protected void afterRequest(HttpServletRequest request, String message) {
+
+    }
+}
